fcc3b92736670c8b4eb6c89956f534e58dce15a9
leevscode <leevscode@gmail.com>
2023-06-13 13:14:20 +0900

    feat: Todo 기본 기능 완료 (state 및 handler 활용)

---
+++ src/components/List.js
@@ -0,0 +1,65 @@
+import React from "react";
+
+const List = () => {
+  const btnStyle = {
+    color: "#fff",
+    float: "right",
+    border: "none",
+    padding: "5px 9px",
+    borderRadius: "50%",
+    cursor: "pointer",
+  };
+  const getStyle = _completed => {
+    return {
+      padding: "10px",
+      borderBottom: "1px dotted #ccc",
+      textDecoration: _completed ? "line-through" : "none",
+      // 기본값이 true이라서 줄이 쳐진다.
+    };
+  };
+  // 이벤트 핸들러
+  const handleClick = _id => {
+    // 전달된 ID를 검색해서 목록에서 제거
+    // 1. 전달된 id로 해당하는 목록 찾기
+    // 2. 새로운 목록으로 갱신해서 화면 리랜더링
+    // 3. 배열의 고차함수 중 filter 를 사용
+    const newTodoData = todoData.filter(item => item.id !== _id);
+    SetTodoData(newTodoData);
+  };
+  const handleCompleteChange = _id => {
+    // 중요한 것은 id에 해당하는 것만 수정 되지 xxx
+    // satae 는 항상 새롭게 만든 내용 즉,  배열로 업데이트 해야 한다.
+    // 새로운 배열을 만들어서 set 하자.
+    let newTodoData = todoData.map(item => {
+      if (item.id === _id) {
+        item.completed = !item.completed;
+        // completed 갱신 및 !는 반대값
+      }
+      return item;
+    });
+    SetTodoData(newTodoData);
+  };
+
+  return (
+    <div>
+      {todoData.map(item => (
+        // key 는 반복문에서 unique 해야 한다
+        <div style={getStyle(item.completed)} key={item.id}>
+          {/* defaultChecked : 체크박스에 기본체크 상태 설정 */}
+          <input
+            type="checkbox"
+            defaultChecked={item.completed}
+            onChange={() => handleCompleteChange(item.id)}
+          />
+          {item.title}
+          {/* 재료(item.id)를 즉시전달하는것을 방지 하기 위해 화살표 함수를 쓴다 */}
+          <button style={btnStyle} onClick={() => handleClick(item.id)}>
+            X
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default List;
