e12c77578e6a0c3d760f775214dcc24b4de68cec
leevscode <leevscode@gmail.com>
2023-06-14 11:50:29 +0900

    feat: todo 목록 전체 삭제기능 추가

--- src/components/Form.js
+++ src/components/Form.js
@@ -39,11 +39,11 @@ const Form = ({ todoData, setTodoData }) => {
           onChange={handleChange}
           className="w-full px-3 py-2 mr-4 text-gray-500 border rounded shadow"
         />
-        <input
+        <input 
           type="submit"
           style={{ flex: "1" }}
           value="입력"
-          className="flex items-center justify-between w-full px-4 py-1 text-gray-600 bg-gray-100 border rounded"
+          className="p-2 text-blue-400 border-2 border-blue-400 rounded hover:text-white hover:bg-blue-400"
         />
       </form>
     </div>
